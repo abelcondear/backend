247e4a5e1aa6468e9624c74c19fdb67f7f6b7a16
A. Conde <abelconde@gmail.com>
2026-05-28 22:18:04 -0300

    added

---
+++ java-pmml4s/pmml4s/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala3-library_3:3.1.3")
+    
+    implementation("org.pmml4s:pmml4s_3:1.5.8")
+
+    implementation("org.rosuda.REngine:Rserve:1.8.1")
+    implementation("org.rosuda.REngine:REngine:2.1.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ java-pmml4s/pmml4s/src/main/java/Program.java
@@ -0,0 +1,136 @@
+import org.pmml4s.model.Model;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.*;
+
+public class Program {
+    private Model model;
+
+    public Program() throws URISyntaxException {
+        //TODO
+    }
+
+    public String GetFilePath(String filename) throws URISyntaxException {
+        URL resource = Program
+                        .class
+                        .getClassLoader()
+                        .getResource(filename);
+        String path;
+
+        if (resource != null) {
+            File file = new File(resource.toURI());
+            path = file.getAbsolutePath();
+        }
+        else {
+            path = "";
+        }
+
+        return path;
+    }
+
+    public Double getRegressionValue(Map<String, Double>
+                                             values) {
+        Object[] valuesMap =
+                Arrays.stream
+                (
+                    model.inputNames()
+                )
+                .map(values::get)
+                .toArray();
+
+        Object[] result = model.predict(valuesMap);
+
+        return (Double) result[0];
+    }
+
+    public void main(String[] args) throws
+            IOException,
+            URISyntaxException, InterruptedException {
+
+        // CSV exists as file source
+        String path = GetFilePath("Elnino.csv");
+        String directory = path.substring(
+                                0,
+                                path.lastIndexOf("\\")
+                            );
+
+        String project_name = "pmml4s";
+
+        // searching project name ("pmml4s")
+        // 10 iterations as maximum iteration
+        for (int x = 0; x < 10; x ++) {
+            if (
+                    directory.indexOf(
+                            "\\" + project_name + "\\",
+                            directory.lastIndexOf("\\")
+                    ) != -1
+            ) {
+                directory = directory.substring(
+                    0,
+                    directory.lastIndexOf("\\")
+                );
+                break;
+            }
+        }
+
+        // This is taken from .\build\resources\main\
+        String filePath = directory + "\\ToPMML.R";
+
+        String commandPath = "c:\\Program Files\\" +
+                            "R\\R-4.5.1\\bin\\x64\\" +
+                            ".\\Rterm";
+
+        ProcessBuilder pb = new ProcessBuilder(
+                commandPath, // command
+                "-f", // file
+                "\"" + filePath + "\"" // file path to be executed
+        );
+        pb.inheritIO();
+        Process p = pb.start();
+
+        // Wait process to be finished
+        int exitCode = p.waitFor();
+
+        System.out.println(" -------------------------- ");
+        System.out.println("Process exit code:" + String.format(" %d", exitCode));
+        System.out.println(" -------------------------- ");
+
+        // ----
+
+        model = Model.fromFile(
+                new File(
+                        GetFilePath("Elnino.pmml")
+                )
+        );
+
+        Map<String, Double> values = Map.of(
+            "buoy_day_ID", 776d,
+            "buoy", 59d,
+            "day", 7d,
+            "latitude", -8.03d,
+            "longitude", 164.82d,
+            "zon_winds", 0d,
+            "mer_winds", 0d,
+            "humidity", 89.50d,
+            "airtemp", 27.51d,
+            "s_s_temp",  0d
+        );
+
+        double predicted = getRegressionValue(values);
+
+        System.out.println(" -------------------------- ");
+        System.out.println(" Predicted value: ");
+        System.out.println(String.format(" %.10f", predicted));
+        System.out.println(" -------------------------- ");
+        System.out.println();
+
+//      ::: Output Console :::
+//      --------------------------
+//       Predicted value:
+//       real value: 29,2236885246
+//       s_s_temp (k10): 29.22d
+//      --------------------------
+
+    }
+}
