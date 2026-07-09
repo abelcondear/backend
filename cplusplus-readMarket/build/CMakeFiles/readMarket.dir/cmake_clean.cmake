4b13faf69b9686e1d0ab83455de1fc482c62b5b8
Abel Conde <abelconde@gmail.com>
2026-07-09 08:41:54 -0300

    added

---
+++ cplusplus-readMarket/build/CMakeFiles/readMarket.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/readMarket.dir/link.d"
+  "CMakeFiles/readMarket.dir/readMarket.cpp.o"
+  "CMakeFiles/readMarket.dir/readMarket.cpp.o.d"
+  "readMarket"
+  "readMarket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/readMarket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
