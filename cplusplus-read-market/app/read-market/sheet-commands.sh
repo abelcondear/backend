ee52e2e2019eba29d52acff89e2364ab01a36888
Abel Conde <abelconde@gmail.com>
2026-08-06 09:13:00 -0300

    added

--- cplusplus-read-market/app/read-market/sheet-commands.sh
+++ cplusplus-read-market/app/read-market/sheet-commands.sh
@@ -12,7 +12,7 @@
 #  cp readMarket.cpp ./build/ && cmake --build ./build --config Release && ./build/readMarket > output.log && nano output.log
 #
 #  g++ readMarket.Generated.cpp -o readMarket.Generated
-#  ./readMarket.Generated
+#  ./readMarket.Generated 5 ";"
 #
 # ---------------------------
 
